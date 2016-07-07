2f07af527944b5032f048aeeb741c66413a67d4b
miguelespada <miguel.valero.espada@gmail.com>
2016-07-07 15:48:41 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('files'));
+
+app.get('/', function(req, res){
+  res.sendfile('barco.html');
+});
+
+app.get('/remo', function(req, res){
+  res.sendfile('remo.html');
+});
+
+
+io.on('connection', function(socket){  
+  socket.on('acc', function(msg){
+    console.log(msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
