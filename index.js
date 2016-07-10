9344e9f072a129e09a14edeae7c683b04dd7301b
Kike-Ramirez <kike@vjspain.com>
2016-07-10 03:39:00 +0200

    ENV file added

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(5000, function(){
-  console.log('listening on *:5000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log(`Listening on ${ process.env.PORT }`);
 });
